2a1b906d53b9f1cbf2a9500acdf161adee455190
steven gonsalvez <steven.gonsalvez@gmail.com>
2022-02-03 10:25:18 +0000

    fix

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const express = require('express');
 // Define Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
+const REGION = process.env.REGION || "mindyourfudgingbusiness"
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// module.exports = {
+//     entry: 'server.js',
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         inject: false,
+//         template: './public/surprise.html',
+
+//         // Pass the full url with the key!
+//         region: `${REGION}`,
+
+//       })
+//     ]
+//   }
 
 // Serve Static Assets
 app.use(express.static('public'));
+
+app.get('/where', function(req, res) {
+    res.send(process.env.REGION || "mindyourfudgingbusiness")
+  });
 
 app.listen(PORT, () => {
     console.log('Server connected at:', PORT);
